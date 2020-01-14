e630af9f07d5fa533fed53a07adeaacbf94e0616
prestonling <pling@ualberta.ca>
2020-01-13 20:02:51 -0700

    initial commit

---
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -0,0 +1,121 @@
+//in Javascript, should only import and export what you need
+// export function getBubbleSortAnimations(array){
+// 	const animations = [];
+
+// }
+
+//if you want to do bubble sort, how would we implement it knowing about
+//pass-by-reference?
+
+//implement switchbars here, then work on bubblesort
+
+// switchBars(array, arrayBars){
+// 	const auxArray = array.slice();
+// 	const arrayBars = document.getElementsByClassName('array-bar');
+// 	const {index} = this.state;
+// 	console.log(index);
+	
+// 	const barOneStyle = arrayBars[index].style;
+// 	const barTwoStyle = arrayBars[index+1].style;
+// 	barOneStyle.height = `${array[index+1]}px`;
+// 	barTwoStyle.height = `${array[index]}px`;
+
+
+// 	//update state array with array with switched values
+// 	auxArray[index] = array[index+1];
+// 	auxArray[index+1] = array[index];
+
+// 	this.setState({
+// 		index: index + 1,
+// 		array: auxArray
+// 	});
+
+// }
+
+export function getSwitchBarsAnimations(array){
+	const animations = [];
+	const auxArray = array.slice();
+
+	for(let i = 0; i < array.length - 1; i ++){
+
+		//push the indexes that we are switching
+		//will change these bars to red
+		animations.push([i, i+1]);
+
+		//push value of left index and its new value
+		animations.push([i, auxArray[i+1]]);
+
+		//push value of right index and its new value
+		animations.push([i+1, auxArray[i]])
+
+		var b = auxArray[i];
+		auxArray[i] = auxArray[i+1];
+		auxArray[i+1] = b; 
+
+		//push the indexes of switched bars
+		//these bars will then turn green
+		animations.push([i, i+1]);
+
+
+	}
+	return animations;
+
+}
+
+export function getBubbleSortAnimations(array){
+	const animations = [];
+	const auxArray = array.slice(); 
+	//go to second last index for first run of bubble sort
+	for(let i = array.length - 2; i > 0; i --){
+		for(let j = 0; j <= i; i ++){
+			
+
+			//push indexes of bars we are comparing
+			//these bars will turn green
+			animations.push([j, j+1]);
+
+			//check if left bar is greater than right bar, and if so, 
+			//push index of left bar and value of right bar
+			//and push the index of the right bar and value of the left bar
+			//if not, just push index and value of left bar respectively 
+			if (auxArray[j]> auxArray[j+1]){
+				animations.push([j, auxArray[j+1]]);
+				animations.push([j+1, auxArray[j]]);
+
+				var b = auxArray[i];
+				auxArray[i] = auxArray[i+1];
+				auxArray[i+1] = b; 
+			}
+			else{
+				animations.push([j, auxArray[j]]);
+				animations.push([j+1, auxArray[j+1]]);
+			}
+
+			//push the indexes of compared bars
+			//these bars will revert back to black
+			animations.push([j, j+1]);
+
+		}
+	}
+}
+
+//should only need array as parameter 
+//-so we use array indexes as values to return
+//for animation array
+//
+
+//bubblesort 
+//need to compare indexes to see if left is greater than right
+//and if so, switch them
+
+//in terms of animations, we can't just have the same pattern
+//as switch bars
+//switchbars has 4 items per cycle
+//-2 for changing the color, and 2 for switching values/height
+
+//bubblesort will also only need 4, but do we keep 
+
+
+
+//what if we returned an array of what indexes were compared?
+//we could just check the values at each index 
---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,200 @@
+import React from 'react';
+import './SortingVisualizer.css';
+import {getSwitchBarsAnimations} from '../SortingAlgorithms/SortingAlgorithms'
+import {getBubbleSortAnimations} from '../SortingAlgorithms/SortingAlgorithms'
+
+const NUMBER_OF_ARRAY_BARS = 10
+
+export default class SortingVisualizer extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			array: [],
+			//index to keep track of which bars have been switched
+			index: 0,
+		}
+	}
+
+	resetArray() {
+	    const array = [];
+	    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+	      array.push(randomIntFromInterval(5, 300));
+	    }
+	    this.setState({array});
+  	}
+
+  	switchBars(){
+  		const {array} = this.state;
+  		const animations = getSwitchBarsAnimations(array);
+  		console.log(animations);
+  		
+  		for(let i = 0; i < animations.length; i++){
+  			const arrayBars = document.getElementsByClassName('array-bar');
+
+
+  			const firstSwitchAnimation = (i % 4 === 1);
+  			const secondSwitchAnimation = (i % 4 === 2);
+
+  			if (firstSwitchAnimation || secondSwitchAnimation){
+  				const [barIdx, newValue] = animations[i];
+  				const bar = arrayBars[barIdx];
+  				//array is being fully changed
+  				//before bars can show animations
+  				setTimeout(() => {
+  					bar.style.height = `${newValue}px`;
+  					bar.innerHTML = `${newValue}`
+  					array[barIdx] = newValue;
+  					this.setState({array});
+  				}, i * 50);
+  				
+
+
+			}
+			else{
+				const [barOneIdx, barTwoIdx] = animations[i];
+				const barOneStyle = arrayBars[barOneIdx].style;
+				const barTwoStyle = arrayBars[barTwoIdx].style;
+				var color = 'black';
+				//change to green if comparing the bars
+				
+				setTimeout(()=> {
+					if (i % 4 === 0){
+						color = 'green';
+					}
+					//ISSUE:color is not reverting back to black
+
+					//change to 
+					else if(i % 4 === 3){
+						color = 'black';
+						console.log("color reverted back to black");
+					}
+					console.log("should be changing colours");
+					barOneStyle.backgroundColor = color;
+					barTwoStyle.backgroundColor = color;
+				}, i * 50);
+			} 
+   		}
+   		console.log(this.state.array);
+  		
+
+
+	}
+	//switch bars now complete
+
+	bubbleSort(){
+		const {array} = this.state;
+		const animations = getBubbleSortAnimations(array);
+		console.log(animations.length);
+
+		for(let i = 0; i < animations.length; i++){
+			const arrayBars = document.getElementsByClassName('array-bar');
+
+			const firstSwitchAnimation = (i % 4 === 1);
+  			const secondSwitchAnimation = (i % 4 === 2);
+
+  			if (firstSwitchAnimation || secondSwitchAnimation){
+  				const [barIdx, newValue] = animations[i];
+  				const bar = arrayBars[barIdx];
+  				//array is being fully changed
+  				//before bars can show animations
+  				setTimeout(() => {
+  					bar.style.height = `${newValue}px`;
+  					bar.innerHTML = `${newValue}`
+  					array[barIdx] = newValue;
+  					this.setState({array});
+  				}, i * 50);
+  				
+
+
+			}
+			else{
+				const [barOneIdx, barTwoIdx] = animations[i];
+				const barOneStyle = arrayBars[barOneIdx].style;
+				const barTwoStyle = arrayBars[barTwoIdx].style;
+				var color = 'black';
+				//change to green if comparing the bars
+				
+				setTimeout(()=> {
+					if (i % 4 === 0){
+						color = 'green';
+					}
+					//ISSUE:color is not reverting back to black
+
+					//change to 
+					else if(i % 4 === 3){
+						color = 'black';
+						console.log("color reverted back to black");
+					}
+					console.log("should be changing colours");
+					barOneStyle.backgroundColor = color;
+					barTwoStyle.backgroundColor = color;
+				}, i * 50);
+			}
+
+
+		}
+	}
+
+	//PROBLEMS
+	//auxBarOneStyle is changing with BarOneStyle
+	//In Javascript, when a variable to assigned to an object
+	//a reference is passed to it.
+	//only primitive types are not references
+
+	//arrays already contain the values of each bar, so just 
+	//update the array 
+
+	//bubble sort should work with this
+
+	//review merge sort, quicksort and heap sort
+
+  	componentDidMount(){
+  		this.resetArray();
+  	}
+	
+	render() {
+    const {array} = this.state;
+
+	    return (
+	      <div className="array-container">
+	        {array.map((value, idx) => (
+	          <div
+	            className="array-bar"
+	            key={idx}
+	            style={{
+	            	backgroundColor: '#000',
+	            	height: `${value}px`,
+	            	color: '#ffffff'
+	            }}>{value}</div>
+	        ))}
+	        <button onClick={() => this.resetArray()}>Generate New Array</button>
+	        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+	        <button onClick={() => this.quickSort()}>Quick Sort</button>
+	        <button onClick={() => this.heapSort()}>Heap Sort</button>
+	        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+	        <button onClick={() => this.testSortingAlgorithms()}>
+	          Test Sorting Algorithms (BROKEN)
+	        </button>
+	        <button onClick={() => this.switchBars()}>Switch items</button>
+	        
+	        
+	      </div>
+	    );
+	}
+}
+
+//function getBubbleSortAnimations
+
+//brainstorm:
+//use for loop to go through an array of arrays of animations 
+//(print a new array every time with changing colors and moving 2 
+//bars everytime)
+
+//test if you can change the height of array items within array, not all 
+//of them
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
